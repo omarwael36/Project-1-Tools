a29ab0bc29b6683512f98999d83afeecea10546a
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-05-22 17:36:34 +0300

    fix order mapping

--- Project1/src/main/java/Entities/Order.java
+++ Project1/src/main/java/Entities/Order.java
@@ -1,10 +1,11 @@
 package Entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Set;
 
 import javax.ejb.Stateless;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,29 +16,35 @@ import javax.persistence.OneToOne;
 
 @Stateless
 @Entity
-public class Order {
+public class Orders implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected int Id;
 	
-	@OneToMany(mappedBy="order" , fetch = FetchType.EAGER)
-	protected Set<Meal> ItemList;
-	protected double Total_Price;
-	protected int fk_runnerId;
-	protected int fk_restaurantId;
-	protected String order_status;
 	
-	@OneToOne(optional=false, mappedBy="order")
+	protected ArrayList<Meal> ItemList;
+	protected double Total_Price;
+	protected String order_status;
+	@OneToOne
+	@JoinColumn(name="fk_runnerId")
 	private Runner runner;
 	
+	@OneToMany(mappedBy="order")
+	private Set<Meal> itemList;
+	
 	@ManyToOne
-	@JoinColumn(name="restaurantID")
+	@JoinColumn(name="fk_restaurantId")
 	private Restaurant restaurant;
 	
-	public Set<Meal> getItemList() {
+	public ArrayList<Meal> getItemList() {
 		return ItemList;
 	}
-	public void setItemList(Set<Meal> itemList) {
+	public void setItemList(ArrayList<Meal> itemList) {
 		ItemList = itemList;
 	}
 	public double getTotal_Price() {
@@ -52,17 +59,5 @@ public class Order {
 	public void setOrder_status(String order_status) {
 		this.order_status = order_status;
 	}
-	public int getFk_runnerId() {
-		return fk_runnerId;
-	}
-	public void setFk_runnerId(int fk_runnerId) {
-		this.fk_runnerId = fk_runnerId;
-	}
-	public int getFk_restaurantId() {
-		return fk_restaurantId;
-	}
-	public void setFk_restaurantId(int fk_restaurantId) {
-		this.fk_restaurantId = fk_restaurantId;
-	}
-	
+
 }
