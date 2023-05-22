a86f6563a2a783a3c8d87ed95f3a818b6b049e53
omarwael36 <119500973+omarwael36@users.noreply.github.com>
2023-05-22 17:35:31 +0300

    fix user mapping

--- Project1/src/main/java/Entities/User.java
+++ Project1/src/main/java/Entities/User.java
@@ -1,31 +1,42 @@
 package Entities;
 
 
+import java.io.Serializable;
+
 import javax.ejb.Stateless;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Stateless
 @Entity
-public class User {
+
+public class User implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected int Id;
-	
 	protected String name;
-	
 	protected String role;
+	protected String password;
 	
-	@OneToOne(optional=false)
-	@JoinColumn(name="restaurantID")
+	
+	@OneToOne(mappedBy="user")
 	private Restaurant restaurant;
 	
 	public User(){
-	
+		
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
 	}
 	public String getName() {
 		return name;
@@ -39,5 +50,12 @@ public class User {
 	public void setRole(String role) {
 		this.role = role;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 	
 }
