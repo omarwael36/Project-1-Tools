f38549005b8d45bb3bdaa53e99a64fd33ca96d21
Ahmed El-Deib <81379336+eldeibb@users.noreply.github.com>
2023-05-22 17:38:22 +0300

    fix runner mapping.java

--- Project1/src/main/java/Entities/Runner.java
+++ Project1/src/main/java/Entities/Runner.java
@@ -1,16 +1,22 @@
 package Entities;
 
+import java.io.Serializable;
+
 import javax.ejb.Stateless;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
 @Stateless
 @Entity
-public class Runner {
+public class Runner implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected int Id;
@@ -21,9 +27,8 @@ public class Runner {
 	
 	protected double delivery_fees;
 	
-	@OneToOne(optional=false)
-	@JoinColumn(name="orderID")
-	private Order order;
+	@OneToOne(mappedBy="runner")
+	private Orders order;
 	
 	public String getName() {
 		return name;
